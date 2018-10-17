3fae237f99c657b3829d2d53286cb26026e08b9f
Kenneth Ng <ng.tszcheong@gmail.com>
2018-10-17 10:45:42 +0800

    Add tutorial codes

--- test.cpp
+++ test.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+using namespace std;
+
+int testFunc() {
+    return 49;
+}
 
 int main(int argc, char const *argv[])
 {
-  std::cout << "Hello Docker container!" << std::endl;
+  cout << "Hello World2" << endl;
+  cout << "Size of char : " << sizeof(char) << endl;
+
+  enum color { red, green, blue } c;
+  c = blue;
+  cout << "c is " << c << endl;
+
+  int testFuncResult = testFunc();
+  cout << "testFuncResult: " << testFuncResult << endl;
+
   return 0;
 }
