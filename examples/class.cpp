d3628e641f63afd4cfc9db7e3734151c7326878c
Kenneth Ng <ng.tszcheong@gmail.com>
2018-10-21 15:24:32 +0800

    tidyup codes

--- examples/class.cpp
+++ examples/class.cpp
@@ -9,47 +9,47 @@ class Box {
         int width;
         int height;
         int length;
-        void setName(string);
-        void setWidth(int);
-        string getName(void);
-        int getVolume(void);
+
         Box(int, int, int); // constructor
+
+        void setName(string);
+        void setWidth(int newWidth) { // inline function
+            this->width = newWidth;
+        };
+
+        string getName(void) {
+            return name;
+        }
+        int getVolume(void) {
+            return width * height * length;
+        }
+        
+        friend void printName(Box box);
 };
 
-Box::Box(int wid, int len, int hgt):width(wid),length(len) // assign width and height member
-{
+Box::Box(int wid, int len, int hgt):width(wid),length(len) { // assign width and height member
     height = hgt * 2;
 }
 
-void Box::setWidth(int newWidth)
-{
-    width = newWidth;
-}
-
-void Box::setName(string newName)
-{
+void Box::setName(string newName) {
     name = newName;
 }
 
-string Box::getName(void)
-{
-    return name;
+// it is a friend function, can access all (include private and protected) members of Box
+void printName(Box box) {
+    cout << "Box name by friend function : " << box.name << endl;
 }
 
-int Box::getVolume(void)
-{
-    return width * height * length;
-}
-
-void classFunctions()
-{
+void classFunctions() {
     Box box1(40, 50, 70);
     box1.setName("hahabox");
 
+    cout << "Height : " << box1.height << endl;
     cout << "Volume : " << box1.getVolume() << endl;
 
     box1.setWidth(90);
     cout << "New volume : " << box1.getVolume() << endl;
-
     cout << "Box1 name : " << box1.getName() << endl;
+
+    printName(box1);
 }
